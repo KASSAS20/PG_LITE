fbd68ebffc721d2b68db822777885326a7f8a973
KASSAS20 <kassas20@mail.ru>
2023-09-09 13:57:43 +0400

    add text in button

--- pg_lite.py
+++ pg_lite.py
@@ -2,9 +2,23 @@ import pygame as pg
 pg.init()
 
 
-def button(root, color, location_size, event):
+def button(root, color, location_size, event, text=False, text_color = False, text_font = False, text_size = 10):
     action = event.type
     but = pg.draw.rect(root, color, location_size)
+
+    if text != False:
+        if text_color == False:
+            text_color = (255-color[0], 255-color[1], 255-color[2])
+        if text_font == False:
+            text_font = 'notosansmonocjkjp'
+
+        font = pg.font.SysFont(text_font, text_size)
+        button_text = font.render(text, True, text_color)
+        but_center = button_text.get_rect(center=but.center)
+        root.blit(button_text, (but_center))
+
+        
+
     if action == pg.MOUSEBUTTONDOWN:
         if event.button == 1:
             position = event.pos
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ root = pg.display.set_mode((500, 500))
 while True:
     for event in pg.event.get():
         action = event.type
-        but = button(root, (255, 0, 0), (10, 10, 30, 30), event)
+        but = button(root, (255, 0, 0), (10, 10, 30, 30), event, text = 'sas')
         if action == pg.QUIT:
             pg.quit()
             exit()

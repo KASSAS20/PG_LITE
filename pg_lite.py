32675c9fedc40e1d8f625a986ee7a5cd33eaae4f
KASSAS20 <kassas20@mail.ru>
2023-09-09 15:02:38 +0400

    check error

--- pg_lite.py
+++ pg_lite.py
@@ -2,15 +2,49 @@ import pygame as pg
 pg.init()
 
 
+def message_error(num, message):
+    print(f'\nEROOR {num} : {message}\n')
+    pg.quit()
+    exit()
+
+
 def button(root, color, location_size, event, text=False, text_color = False, text_font = False, text_size = 10):
+    if not isinstance(root, pg.surface.Surface):
+        message_error('1', 'root is specified incorrectly')
+    elif not isinstance(color, tuple):
+        message_error(
+            '2', 'the color of the button should be transmitted as a tuple')
+    elif len(color) != 3:
+        message_error('3', 'the color must be transmitted in RGB format')
+    elif not isinstance(location_size, tuple):
+        message_error(
+            '4', 'the location_size of the button should be transmitted as a tuple')
+    elif len(location_size) != 4:
+        message_error(
+            '5', 'location_size must be passed in the format (x, y, width, height)')
+    elif not isinstance(event, pg.event.Event):
+        message_error('6', 'event is specified incorrectly')
+    
+    
+
+
     action = event.type
     but = pg.draw.rect(root, color, location_size)
 
     if text != False:
+        text = str(text)
         if text_color == False:
             text_color = (255-color[0], 255-color[1], 255-color[2])
         if text_font == False:
             text_font = 'notosansmonocjkjp'
+
+        elif text_color not in pg.font.get_fonts():
+            message_error('7', 'The font you entered was not found on your PC')
+        elif not isinstance(text_color, tuple):
+            message_error(
+            '8', 'the text_color of the button should be transmitted as a tuple')
+        elif len(color) != 3:
+            message_error('9', 'the text_color must be transmitted in RGB format')
 
         font = pg.font.SysFont(text_font, text_size)
         button_text = font.render(text, True, text_color)
--- test.py
+++ test.py
@@ -8,10 +8,19 @@ root = pg.display.set_mode((500, 500))
 while True:
     for event in pg.event.get():
         action = event.type
-        but = button(root, (255, 0, 0), (10, 10, 30, 30), event, text = 'sas')
+        root.fill((0,0,0))
+        but = button(root, (255, 0, 0), (10, 10, 50, 50), event, text = 1)
+        but_2 = button(root, (255, 0, 0), (50, 50, 50, 30), event, text = '2')
+        but_3 = button(root, (255, 0, 0), (90, 90, 30, 50), event, text = '3')
+
+        
         if action == pg.QUIT:
             pg.quit()
             exit()
         if but:
-            print('press')
+            print('press 1')
+        elif but_2:
+            print('press 2')
+        elif but_3:
+            print('press 3')
     pg.display.flip()
